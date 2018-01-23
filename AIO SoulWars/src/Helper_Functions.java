fc9533f17a374ed688a4beb05e4906399361e44e
BuildTools <unconfigured@null.spigotmc.org>
2018-01-22 22:59:23 -0500

    Added area constants

--- AIO SoulWars/src/Helper_Functions.java
+++ AIO SoulWars/src/Helper_Functions.java
@@ -1,17 +1,27 @@
+import xobot.script.wrappers.Area;
+
 /**
  * Created by SRH on 1/22/2018.
  */
 
 public class Helper_Functions {
     public enum State {
-        GHOST, WALKING_TO_AVATAR,
+        GHOST,
+        WALKING_TO_AVATAR,
         WALKING_TO_WEST_GRAVEYARD,
         WALKING_TO_EAST_GRAVEYARD,
-        WALKING_TO_MIDDLE, FIGHTING,
+        WALKING_TO_MIDDLE,
+        FIGHTING,
         WALKING_TO_JELLIES,
         WALKING_TO_PYREFIENDS,
         COLLECTING_SHARDS,
         USING_SHARDS_ON_OBLISK
         //More states will probably need to be added.
     }
+
+    public Area blueSpawnArea = new Area(0, 0, 0, 0);
+    public Area redSpawnArea = new Area(0, 0, 0, 0);
+    public Area eastGraveyardArea = new Area(0, 0, 0, 0);
+    public Area westGraveyardArea = new Area(0, 0, 0, 0);
+    public Area obliskArea = new Area(0, 0, 0, 0);
 }
