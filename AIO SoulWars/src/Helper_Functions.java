66e650cfbf22e9b502eda0f02208f814a950de0e
BuildTools <unconfigured@null.spigotmc.org>
2018-01-22 22:26:28 -0500

    Added handleState()

---
+++ AIO SoulWars/src/Helper_Functions.java
@@ -0,0 +1,17 @@
+/**
+ * Created by SRH on 1/22/2018.
+ */
+
+public class Helper_Functions {
+    public enum State {
+        GHOST, WALKING_TO_AVATAR,
+        WALKING_TO_WEST_GRAVEYARD,
+        WALKING_TO_EAST_GRAVEYARD,
+        WALKING_TO_MIDDLE, FIGHTING,
+        WALKING_TO_JELLIES,
+        WALKING_TO_PYREFIENDS,
+        COLLECTING_SHARDS,
+        USING_SHARDS_ON_OBLISK
+        //More states will probably need to be added.
+    }
+}
---
+++ AIO SoulWars/src/Paint.java
@@ -0,0 +1,13 @@
+import xobot.client.callback.listeners.PaintListener;
+
+import java.awt.*;
+
+/**
+ * Created by SRH on 1/22/2018.
+ */
+public class Paint implements PaintListener {
+    @Override
+    public void repaint(Graphics graphics) {
+
+    }
+}
