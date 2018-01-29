058374afdadd85052d9c7c742b6640faeaa74648
BuildTools <dcress01@uoguelph.ca>
2018-01-29 14:37:55 -0500

    Added a bunch of constants, fixed Dead.java a bit

--- AIO SoulWars/src/aids/Constants.java
+++ AIO SoulWars/src/aids/Constants.java
@@ -12,10 +12,12 @@ public class Constants {
     }
 
     public static final Area BLUE_SPAWN_AREA = new Area(0, 0, 0, 0);
-    public static final Area RED_SPAWN_AREA = new Area(0, 0, 0, 0);
-    public static final Area EAST_GRAVEUARD_AREA = new Area(0, 0, 0, 0);
-    public static final Area WEST_GRAVEYWARD_AREA = new Area(0, 0, 0, 0);
-    public static final Area OBLISK_AREA = new Area(0, 0, 0, 0);
+    public static final Area RED_SPAWN_AREA = new Area(1958, 3244, 1951, 3241);
+    public static final Area EAST_GRAVEYARD_SPAWN = new Area(1932, 3246, 1934, 3244);
+    public static final Area WEST_GRAVEYARD_SPAWN = new Area(1841, 3217, 1843, 3219);
+    public static final Area EAST_GRAVEUARD_AREA = new Area(1938, 3240, 1928, 3250);
+    public static final Area WEST_GRAVEYWARD_AREA = new Area(1837, 3223, 1847, 3213);
+    public static final Area OBLISK_AREA = new Area(1894, 3239, 1879, 3224);
 
 
     public static final Area LOBBY_AREA = new Area(new Tile(1872, 1904), new Tile(3140, 3186));
@@ -27,8 +29,9 @@ public class Constants {
     public static final int BANDAGE_STALL = 42024;
     public static final int PYREFIEND = 1636;
     public static final int SHARD_FRAGMENT = 14639;
+    public static final int OBLISK = -1;
 
-    public static final int EXIT_BARRIER = -1;
+    public static final int[] EXIT_BARRIER = {42020, 42019, 42018};
 
     public static final int BANDAGES = 4049;
 
--- AIO SoulWars/src/nodes/Dead.java
+++ AIO SoulWars/src/nodes/Dead.java
@@ -4,8 +4,11 @@ import aids.Constants;
 import aids.Helper_Functions;
 import aids.Variables;
 import xobot.script.methods.GameObjects;
+import xobot.script.methods.Players;
 import xobot.script.util.Time;
 import xobot.script.wrappers.interactive.GameObject;
+
+import static aids.Constants.*;
 
 /**
  * Created by Cyn on 1/23/2018.
@@ -14,17 +17,24 @@ public class Dead extends Node {
     Helper_Functions help;
     @Override
     public boolean validate() {
-        return help.inEastGraveyardArea()
-                || help.inWestGraveyardArea()
-                || help.inSpawnArea();
+        return BLUE_SPAWN_AREA.contains(Players.getMyPlayer().getLocation())
+                || RED_SPAWN_AREA.contains(Players.getMyPlayer().getLocation())
+                || WEST_GRAVEYARD_SPAWN.contains(Players.getMyPlayer().getLocation())
+                || EAST_GRAVEYARD_SPAWN.contains(Players.getMyPlayer().getLocation());
     }
 
     @Override
     public void execute() {
         Variables.setCurrentNode(this);
 
-        GameObject barrier = GameObjects.getNearest(Constants.EXIT_BARRIER);//GET EXIT_BARRIER VALUE
-        barrier.interact("Pass");//DOUBLE CHECK STRING
+        for (int i : EXIT_BARRIER)
+        {
+            GameObject barrier = GameObjects.getNearest(i);
+            if (barrier != null && barrier.getDistance() < 15)
+            {
+                barrier.interact("Pass");//DOUBLE CHECK STRING
+            }
+        }
         Time.sleep(1200);
     }
 
